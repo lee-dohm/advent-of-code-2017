2f7ad10580a6b4915d5a68eab9fc6901ab40219d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-09 10:09:47 -0800

    Add Day 3 Part 1 solution and documentation of Part 2

--- day-3/day-3-test.exs
+++ day-3/day-3-test.exs
@@ -7,5 +7,52 @@ defmodule Day3.Test do
   use ExUnit.Case
 
   describe "first half solution" do
+    test "next_ring with no input is the first ring" do
+      assert Day3.next_ring() == {1, 1, 0}
+    end
+
+    test "next_ring with the first ring is ring 1" do
+      assert Day3.next_ring({1, 1, 0}) == {2, 9, 1}
+    end
+
+    test "next_ring with ring 1 is ring 2" do
+      assert Day3.next_ring({2, 9, 1}) == {10, 25, 2}
+    end
+
+    test "in ring 2, 10 is subrange 0 index 0" do
+      assert Day3.get_subrange_indices({10, 25, 2}, 10) == {0, 0}
+    end
+
+    test "in ring 2, 23 is subrange 6 index 1" do
+      assert Day3.get_subrange_indices({10, 25, 2}, 23) == {6, 1}
+    end
+
+    test "in ring 2, subrange 0 is 10..11" do
+      assert Day3.get_subrange({10, 25, 2}, 0) == {10, 11}
+    end
+
+    test "in ring 2, subrange 6 is 22..23" do
+      assert Day3.get_subrange({10, 25, 2}, 6) == {22, 23}
+    end
+
+    test "distance_to_side_center for ring 2 subrange 0 index 0 is 1" do
+      assert Day3.distance_to_side_center(2, {0, 0}) == 1
+    end
+
+    test "distance_to_side_center for ring 2 subrange 5 index 1 is 2" do
+      assert Day3.distance_to_side_center(2, {5, 1}) == 2
+    end
+
+    test "distance_to_center for ring 2 subrange 0 index 0 is 3" do
+      assert Day3.distance_to_center(2, {0, 0}) == 3
+    end
+
+    test "find_ring(1) is ring 0" do
+      assert Day3.find_ring(1) == {1, 1, 0}
+    end
+
+    test "find_ring(23) is ring 2" do
+      assert Day3.find_ring(23) == {10, 25, 2}
+    end
   end
 end
--- day-3/day-3.exs
+++ day-3/day-3.exs
@@ -1,2 +1,61 @@
 defmodule Day3 do
+  @moduledoc """
+  The way the spiral memory is designed:
+
+  * Each "ring" is 8 cells larger than the last
+  * A ring contains a contiguous range of integers, so it can be expressed as `x..y`
+  * A ring has four sides, that contain a list of integers
+
+  To determine the Manhattan distance, we need to:
+
+  * Find the ring that contains the target number
+      * Generate the range of each ring as `old_max + 1 .. old_max + 8 * n`
+      * Return the ring number and the range extents
+  * Find the side of that ring that contains the target number
+      * Divide the range into eight sub-ranges, each `n` long
+      * Each sub-range is `current_min + sub_range_number * n .. currennt_min + sub_range_number * n + n`
+      * `(target - current_min) div n` gives us the sub-range number
+      * `(target - current_min) mod n` gives us the index within the sub-range
+  * Count from that target number's cell to the center of that side
+      * If the sub-range number is even, `n - index - 1` is the number of steps to the center
+      * If the sub-range number is odd, `n`
+  * Count the number of rings minus the centermost ring
+      * `n`
+  """
+
+  def distance_to_center(ring, subrange_pos) do
+    distance_to_side_center(ring, subrange_pos) + ring
+  end
+
+  def distance_to_side_center(ring, {subrange, index}) when rem(subrange, 2) == 0 do
+    ring - index - 1
+  end
+
+  def distance_to_side_center(_ring, {_subrange, index}) do
+    index + 1
+  end
+
+  def find_ring(1), do: {1, 1, 0}
+
+  def find_ring(target) do
+    [ring] = Stream.iterate({1, 1, 0}, &next_ring/1)
+             |> Enum.take_while(fn({min, _, _}) -> min < target end)
+             |> Enum.slice(-1..-1)
+
+    ring
+  end
+
+  def get_subrange({min, _, n}, index) do
+    {min + index * n, min + (index + 1) * n - 1}
+  end
+
+  def get_subrange_indices({min, _, n}, target) do
+    {div(target - min, n), rem(target - min, n)}
+  end
+
+  def next_ring(last_ring \\ nil)
+  def next_ring(nil), do: {1, 1, 0}
+  def next_ring({_, old_max, index}) do
+    {old_max + 1, old_max + 8 * (index + 1), index + 1}
+  end
 end
